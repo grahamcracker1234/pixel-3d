712a559b0798a1213640ef956c4459641a362c7f
Graham Preston <g.b.preston@proton.me>
2024-01-10 16:11:12 -0500

    Better instancing

--- Assets/Scripts/GrassInstancer.cs
+++ Assets/Scripts/GrassInstancer.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class GrassInstancer : MonoBehaviour
 {
-    // [SerializeField] int _size = 10;
     [SerializeField] int _density = 10;
-    [SerializeField] Mesh mesh;
-    [SerializeField] Material material;
+    [SerializeField] Mesh _mesh;
+    [SerializeField] Material _material;
+    [SerializeField] int _seed = 42;
+
+    MeshGenerator _meshGenerator;
 
     Matrix4x4[] matrices;
     float _size;
 
     void OnEnable()
     {
+        _meshGenerator = GetComponent<MeshGenerator>();
         var size = GetComponent<MeshRenderer>().bounds.size;
         Assert.IsTrue(size.x == size.z);
         _size = size.x;
@@ -22,24 +26,39 @@ public class GrassInstancer : MonoBehaviour
 
     void Update()
     {
+        Random.InitState(_seed);
+
         // Matrix4x4 P = Camera.main.projectionMatrix;
         // Matrix4x4 V = Camera.main.transform.worldToLocalMatrix;
         // Matrix4x4 VP = P * V;
 
         matrices = new Matrix4x4[_density * _density];
 
-        for (int x = 0; x < _density; x++)
+        var index = 0;
+        for (int y = 0; y < _density; y++)
         {
-            for (int y = 0; y < _density; y++)
+            for (int x = 0; x < _density; x++)
             {
-                var index = x + y * _density;
-                var offset = (float)_density / 2 - 0.5f;
-                var position = new Vector3(x - offset, 0, y - offset) * _size / _density + transform.position;
-                var rotation = Quaternion.LookRotation(position - Camera.main.transform.position, Vector3.up);
+
+                var centeringOffset = (float)_density / 2 - 0.5f;
+                var scale = _size / _density;
+
+                var offset = Random.insideUnitCircle * scale;
+                var position = new Vector3(x - centeringOffset, 0, y - centeringOffset) * scale + transform.position + new Vector3(offset.x, 0, offset.y);
+
+                var uv = new Vector2(x, y) / _density;
+                position.y = _meshGenerator.GetMeshHeight(uv);
+
+                var target = Camera.main.transform.position;
+                var rotation = Quaternion.LookRotation(position - target, Vector3.up);
+
                 matrices[index] = Matrix4x4.TRS(position, rotation, Vector3.one);
+
+                index++;
             }
         }
 
-        Graphics.DrawMeshInstanced(mesh, 0, material, matrices);
+        var rp = new RenderParams(_material);
+        Graphics.RenderMeshInstanced(rp, _mesh, 0, matrices);
     }
 }
---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.PackageManager.UI;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshCollider))]
+public class MeshGenerator : MonoBehaviour
+{
+	public Texture2D heightMap;
+	public Vector3 size = new Vector3(10, 3, 10);
+	public Vector2Int sampleCount = new Vector2Int(25, 25);
+
+	public MeshData GenerateTerrainMesh()
+	{
+		var meshData = new MeshData(sampleCount.x, sampleCount.y);
+
+		var index = 0;
+		for (var y = 0; y < sampleCount.y; y++)
+		{
+			for (var x = 0; x < sampleCount.x; x++)
+			{
+				var uv = new Vector2(x, y) / (sampleCount - Vector2.one);
+				var heightValue = GetMeshHeight(uv);
+
+				var vertex = (uv - Vector2.one / 2) * new Vector2(size.x, size.z); ;
+
+				meshData.vertices[index] = new Vector3(vertex.x, heightValue, vertex.y);
+				meshData.uvs[index] = uv;
+
+				if (x < sampleCount.x - 1 && y < sampleCount.y - 1)
+				{
+					meshData.AddTriangle(index, index + sampleCount.x + 1, index + sampleCount.x);
+					meshData.AddTriangle(index + sampleCount.x + 1, index, index + 1);
+				}
+
+				index++;
+			}
+		}
+
+		var meshFiler = GetComponent<MeshFilter>();
+		meshFiler.sharedMesh = meshData.CreateMesh();
+		GetComponent<MeshCollider>().sharedMesh = meshFiler.sharedMesh;
+
+		return meshData;
+	}
+
+	public float GetMeshHeight(Vector2 uv)
+	{
+		var x = Mathf.RoundToInt(uv.x * heightMap.width);
+		var y = Mathf.RoundToInt(uv.y * heightMap.height);
+		return GetMeshHeight(x, y);
+	}
+
+	public float GetMeshHeight(int x, int y)
+	{
+		return heightMap.GetPixel(x, y).grayscale * size.y;
+	}
+}
+
+public class MeshData
+{
+	public Vector3[] vertices;
+	public int[] triangles;
+	public Vector2[] uvs;
+
+	int triangleIndex;
+
+	public MeshData(int meshWidth, int meshHeight)
+	{
+		vertices = new Vector3[meshWidth * meshHeight];
+		uvs = new Vector2[meshWidth * meshHeight];
+		triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+	}
+
+	public void AddTriangle(int a, int b, int c)
+	{
+		triangles[triangleIndex] = a;
+		triangles[triangleIndex + 1] = c;
+		triangles[triangleIndex + 2] = b;
+		triangleIndex += 3;
+	}
+
+	public Mesh CreateMesh()
+	{
+		Mesh mesh = new Mesh
+		{
+			vertices = vertices,
+			triangles = triangles,
+			uv = uvs
+		};
+		mesh.RecalculateNormals();
+		return mesh;
+	}
+
+}
+
+[CustomEditor(typeof(MeshGenerator))]
+public class MeshGeneratorEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		var script = (MeshGenerator)target;
+		if (GUILayout.Button("Generate Terrain"))
+		{
+			script.GenerateTerrainMesh();
+		}
+	}
+}
